636115656d1d873342bd15b68acc34047c8db15f
Motoki4472 <bonjour4472@gmail.com>
2025-02-11 17:47:15 +0900

    盤上のワードブロックを作成

---
+++ Assets/Scripts/WordBlock/WordBlockOnBoard.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WordBlock
+{
+    public class WordBlockOnBoard
+    {
+        private int[] BoardCoordinate = new int[2];
+        private string Word;
+
+        public WordBlockOnBoard(int x, int y, string word)
+        {
+            BoardCoordinate[0] = x;
+            BoardCoordinate[1] = y;
+            Word = word;
+        }
+
+        public void SetBoardCoordinate(int x, int y)
+        {
+            BoardCoordinate[0] = x;
+            BoardCoordinate[1] = y;
+        }
+
+        public void SetWord(string word)
+        {
+            Word = word;
+        }
+
+        
+
+        public int[] GetBoardCoordinate()
+        {
+            return BoardCoordinate;
+        }
+
+        public string GetWord()
+        {
+            return Word;
+        }
+
+
+    }
+}
