ac7dc033e30335ba95450c035104c629da12ab90
Motoki4472 <bonjour4472@gmail.com>
2025-02-12 19:09:46 +0900

    scoreを一通り実装

---
+++ Assets/Scripts/Rule/ScoreSystem.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+namespace Assets.Rule
+{
+    public class ScoreSystem
+    {
+        private int score = 0;
+        private float WordCountFactor = 1.0f;
+        private float TurnCountFactor = 1.0f;
+        private int ComboCount = 0;
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public int GetComboCount()
+        {
+            return ComboCount;
+        }
+
+        public void AddScore()
+        {
+            score += (int)(100 * WordCountFactor * TurnCountFactor);
+        }
+
+        public void AddComboCount()
+        {
+            ComboCount++;
+        }
+
+        public void ResetComboCount()
+        {
+            ComboCount = 0;
+        }
+
+        public void AddWordCountFactor()
+        {
+            WordCountFactor += 0.1f;
+        }
+
+        public void ResetWordCountFactor()
+        {
+            WordCountFactor = 1.0f;
+        }
+
+        public void AddTurnCountFactor()
+        {
+            TurnCountFactor += 0.025f;
+        }
+
+        public void ResetTurnCountFactor()
+        {
+            TurnCountFactor = 1.0f;
+        }
+
+        public void ResetAll()
+        {
+            score = 0;
+            WordCountFactor = 1.0f;
+            TurnCountFactor = 1.0f;
+            ComboCount = 0;
+        }
+
+
+    }
+}
