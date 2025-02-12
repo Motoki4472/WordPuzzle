8ff08a2282df34ab24258d4904bf0a64d6a352f1
Motoki4472 <bonjour4472@gmail.com>
2025-02-12 16:36:19 +0900

    コンソールで動作するまで

--- Assets/Scripts/Rule/GameRule.cs
+++ Assets/Scripts/Rule/GameRule.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.WordBlock;
+using Random = UnityEngine.Random;
 
 namespace Assets.Rule
 {
@@ -10,9 +11,12 @@ namespace Assets.Rule
     {
         private WordBlockOnBoard[,] Board = new WordBlockOnBoard[5, 5];
         private List<WordBlockOnBoard[]> ConnectList = new List<WordBlockOnBoard[]>();
+        private WordList WordList = new WordList();
+        private ProcessSystem processSystem;
 
-        public GameRule()
+        public GameRule(ProcessSystem processSystem)
         {
+            this.processSystem = processSystem;
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
@@ -21,7 +25,35 @@ namespace Assets.Rule
                 }
             }
 
+            Board[0, 0].SetWord("a");
+            //Debug.Log(Board[0, 0].GetWord() + " " + Board[0, 0].GetWordId());
+
             //初期配置
+            // logに出力
+            DebugLogBoard();
+        }
+
+        private void DebugLogBoard()
+        {
+            string[] raw = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                string row = "[";
+                for (int j = 0; j < 5; j++)
+                {
+                    if (Board[i, j].GetWord() == null)
+                    {
+                        row += " ][";
+                    }
+                    else
+                    {
+                        row += " " + Board[i, j].GetWord() + "][";
+                    }
+                }
+                row = row.TrimEnd('['); // 最後の '[' を削除
+                raw[i] = row;
+            }
+            Debug.Log(raw[0] + "\n" + raw[1] + "\n" + raw[2] + "\n" + raw[3] + "\n" + raw[4]);
         }
 
         //盤面の操作
@@ -35,9 +67,19 @@ namespace Assets.Rule
                     {
                         Board[j, i].SetWord(Board[j + 1, i].GetWord());
                         Board[j + 1, i].SetWord(null);
+
+                        // ここで上にずれるアニメーションを入れる
                     }
                 }
             }
+            //最下段にランダムな文字を生成
+
+            int RandomX = Random.Range(0, 5);
+            if (Board[RandomX, 4].GetWord() == null)
+            {
+                GenerateRandomWordBlock(RandomX, 4);
+            }
+
             CheckConnect();
         }
 
@@ -51,8 +93,17 @@ namespace Assets.Rule
                     {
                         Board[j, i].SetWord(Board[j - 1, i].GetWord());
                         Board[j - 1, i].SetWord(null);
+
+                        // ここで下にずれるアニメーションを入れる
                     }
                 }
+            }
+
+            //最上段にランダムな文字を生成
+            int RandomX = Random.Range(0, 5);
+            if (Board[RandomX, 0].GetWord() == null)
+            {
+                GenerateRandomWordBlock(RandomX, 0);
             }
             CheckConnect();
         }
@@ -67,8 +118,17 @@ namespace Assets.Rule
                     {
                         Board[i, j] = Board[i, j - 1];
                         Board[i, j - 1].SetWord(null);
+
+                        // ここで右にずれるアニメーションを入れる
                     }
                 }
+            }
+
+            // 最左列にランダムな文字を生成
+            int RandomY = Random.Range(0, 5);
+            if (Board[0, RandomY].GetWord() == null)
+            {
+                GenerateRandomWordBlock(0, RandomY);
             }
             CheckConnect();
         }
@@ -83,10 +143,28 @@ namespace Assets.Rule
                     {
                         Board[i, j] = Board[i, j + 1];
                         Board[i, j + 1].SetWord(null);
+
+                        // ここで左にずれるアニメーションを入れる
                     }
                 }
             }
+
+            // 最右列にランダムな文字を生成
+            int RandomY = Random.Range(0, 5);
+            if (Board[4, RandomY].GetWord() == null)
+            {
+                GenerateRandomWordBlock(4, RandomY);
+            }
             CheckConnect();
+        }
+
+        private void GenerateRandomWordBlock(int x, int y)
+        {
+            int wordId = Random.Range(0, 25);
+            string word = ((char)(wordId + 97)).ToString();
+            //Debug.Log(word);
+            Board[x, y].SetWord(word);
+            //Debug.Log(Board[x, y].GetWord() + " " + Board[x, y].GetBoardCoordinate()[0] + " " + Board[x, y].GetBoardCoordinate()[1]);
         }
 
         //盤面内の文字のつながりを検査
@@ -100,7 +178,7 @@ namespace Assets.Rule
                 CheckConnectVertical(i);
             }
 
-
+            DeleteWords();
         }
 
         // 横方向のつながりの検査
@@ -118,7 +196,7 @@ namespace Assets.Rule
                 }
                 if (connectCount >= 2)
                 {
-                    for(int j = 0;j < connectCount;j++)
+                    for (int j = 0; j < connectCount; j++)
                     {
                         connect[j] = Board[i - connectCount + j + 1, y];
                     }
@@ -151,7 +229,36 @@ namespace Assets.Rule
             }
         }
 
-        // word list と照らし合わせて該当部分を消去
+        private void DeleteWords()
+        {
+            // ConnectListを文字数多い順にソート
+            ConnectList.Sort((a, b) => b.Length - a.Length);
+            // 各要素について辞書と照らし合わせて消す
+            for (int i = 0; i < ConnectList.Count; i++)
+            {
+                string word = "";
+                for (int j = 0; j < ConnectList[i].Length; j++)
+                {
+                    if (ConnectList[i][j] != null) // null チェックを追加
+                    {
+                        word += ConnectList[i][j].GetWord();
+                    }
+                }
+                if (WordList.IsExistWord(word))
+                {
+                    for (int j = 0; j < ConnectList[i].Length; j++)
+                    {
+                        if (ConnectList[i][j] != null)
+                        {
+                            ConnectList[i][j].SetWord(null);
+                            // ここで消えるアニメーションを入れる
+                        }
+                    }
+                }
+            }
+            DebugLogBoard();
+            processSystem.SetProcessStateToRunning();
+        }
 
     }
 }
